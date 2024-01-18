b46b64b40a0cc86281e78e4098368031cd1cc78a
shamnasherinkk <156903418+shamnasherinkk@users.noreply.github.com>
2024-01-18 10:20:04 +0000

    calculator

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
-from flask import Flask
-flask,render_template,request
+from flask import Flask,render_template,request
 app=Flask(__name__)
 @app.route('/')
 def hello():
-return render_template('index.html')
+    return render_template('index.html')
 
 @app.route("/result", methods=['GET', 'POST'])
 def hi():
